6ef33e4d697951b82db8d560043aad9a92edcf0a
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-12-13 16:09:53 +0100

    Added test

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once("vendor/autoload.php");
 
 use MBO\DBManager;
-use MBO\MBOBuilder;
 
 global $configPath;
-$configPath = "config/config.json";
+$configPath = "./config/config.json";
 
 $a = new DBManager($configPath);
-
-$b = new MBOBuilder();
-$b->SELECT("id", "firstname");
