d063e13b74ca0dc5d4e3da3f8c28b69a65e4ff0e
Strikes <yanis.bendahmane@supinternet.fr>
2018-12-13 16:26:06 +0100

    Added DELETE, UPDATE and INSERT in builder

--- src/MBO/MBOBuilder.php
+++ src/MBO/MBOBuilder.php
@@ -6,10 +6,34 @@ class MBOBuilder
 {
     private $select = [];
 
+    private $delete = [];
+
+    private $update = [];
+
+    private $insert = [];
+
     public function SELECT(...$selected)
     {
         $this->setSelect(array_push($this->getSelect(), $selected));
+        return $this;
+    }
+
+    public function DELETE(...$deleted)
+    {
+        $this->setDelete(array_push($this->getDelete(), $deleted));
         var_dump($this->getSelect());
+    }
+
+    public function UPDATE(...$updated)
+    {
+        $this->setInsert(array_push($this->getUpdate(), $selected));
+        return $this;
+    }
+
+    public function INSERT(...$inserted)
+    {
+        $this->setInsert(array_push($this->getInsert(), $inserte));
+        return $this;
     }
 
     public function getSelect()
@@ -20,6 +44,39 @@ class MBOBuilder
     public function setSelect($select)
     {
         $this->select = $select;
+        return $this;
+    }
+
+    public function getDelete()
+    {
+        return $this->delete;
+    }
+
+    public function setDelete($delete)
+    {
+        $this->delete = $delete;
+        return $this;
+    }
+
+    public function getUpdate()
+    {
+        return $this->update;
+    }
+
+    public function setUpdate($update)
+    {
+        $this->update = $update;
+        return $this;
+    }
+
+    public function getInsert()
+    {
+        return $this->insert;
+    }
+
+    public function setInsert($insert)
+    {
+        $this->insert = $insert;
         return $this;
     }
 }
