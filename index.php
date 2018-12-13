04a62e3bfd7fee85ad41790ec63aac1334f1c5af
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-12-13 14:38:12 +0100

    added config.json in git ignore

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 13/12/2018
+ * Time: 14:30
+ */
---
+++ src/DBManager.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 13/12/2018
+ * Time: 14:19
+ */
+
+class DBManager
+{
+
+}
