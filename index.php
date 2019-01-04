ee64ef92782a53acac358db4b09dc41b53cbb7dc
Maxime Maréchal <maxime.marechal@supinternet.fr>
2019-01-04 15:12:32 +0100

    getById return instance of entity with good value

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ $GLOBALS['config'] = "./config/config.json";
 $clients = new clients();
 //$clients->INSERT(["lastname", "Max"], ["firstname", "ime"], ["address", "adres"], ["city", "citi"], ["country_id", 42], ["phone", "0102030405"], ["email", "e@ma.il"]);
 //$clients->UPDATE(["lastname", "Maxx"], ["qdtht", "qfhdqfs"], ["address", "slt"]);
-$clients->SELECT("city", "phone");
-$clients->COUNT("lastname", true)
-    ->COUNT("dfgsdfh")
-    ->COUNT("firstname");
-var_dump($clients->getCount());
-$clients->buildQuery();
-var_dump($clients->getQuery());
-var_dump($clients->execute());
+//$clients->SELECT("city", "phone");
+//$clients->COUNT("lastname", true)
+//    ->COUNT("dfgsdfh")
+//    ->COUNT("firstname");
+//var_dump($clients->getCount());
+//$clients->buildQuery();
+//var_dump($clients->getQuery());
+//var_dump($clients->execute());
+$clients->getById(3);
+var_dump($clients);
--- src/MBO/MBOEntity.php
+++ src/MBO/MBOEntity.php
@@ -9,13 +9,27 @@ abstract class MBOEntity extends MBOBuilder implements EntityInterface
         parent::__construct();
     }
 
+    public function buildEntity(array $data) {
+        foreach ($this->getCol() as $col) {
+            $func = 'set' . ucfirst($col);
+            $pos = strpos($func, '_');
+            while (!($pos === false)) {
+                $func[$pos + 1] = ucfirst($func[$pos + 1]);
+                $func = substr($func, 0, $pos) . substr($func, $pos + 1);
+                $pos = strpos($func, '_');
+            }
+            $this->$func($data[$col]);
+        }
+        return $this;
+    }
+
     public function getById($id, $fetchStyle = 2) {
         $this
             ->clear()
             ->SELECT('*')
             ->WHERE(['id', '=', $id])
             ->buildQuery();
-        return $this->execute($fetchStyle);
+        return $this->buildEntity($this->execute($fetchStyle)[0]);
     }
 
 }
